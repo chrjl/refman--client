b645f5c2da02ced7c1bfa8d59ce81ce7db4f01fd
chrjl <73675826+chrjl@users.noreply.github.com>
2023-03-01 23:51:35 -0800

    Build selector widget that utilizes idb

--- static/dev/idb.js
+++ static/dev/idb.js
@@ -2,20 +2,18 @@ import { openDB } from 'https://cdn.jsdelivr.net/npm/idb@7/+esm';
 
 const idb = {};
 
-idb.app = openDB('app', 1, {
+idb.app = await openDB('app', 1, {
   upgrade(db) {
-    db.createObjectStore('entries', { keyPath: 'id' });
+    if (!db.objectStoreNames.contains('entries')) {
+      const entriesStore = db.createObjectStore('entries', { keyPath: 'id' });
+      entriesStore.createIndex('keyword', 'keywords', { multiEntry: true });
+    }
+
+    if (!db.objectStoreNames.contains('upload')) {
+      const entriesStore = db.createObjectStore('upload', { keyPath: 'id' });
+      entriesStore.createIndex('keyword', 'keywords', { multiEntry: true });
+    }
   },
 });
-
-idb.app.clearEntries = async () => {
-  const db = await idb.app;
-  db.clear('entries');
-};
-
-idb.app.putEntries = async (data) => {
-  const db = await idb.app;
-  data.forEach((entry) => db.put('entries', entry));
-};
 
 export default idb;
