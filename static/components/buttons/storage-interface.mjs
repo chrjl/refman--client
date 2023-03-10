e98b5a1de15d5a7c3c893a0585877156ae597a9a
chrjl <73675826+chrjl@users.noreply.github.com>
2023-03-09 21:04:46 -0800

    updated definition of env (endpoint URLs)

--- static/components/buttons/storage-interface.mjs
+++ static/components/buttons/storage-interface.mjs
@@ -6,15 +6,16 @@ export default class extends DocumentFragment {
     this.endpoint = options.endpoint;
 
     this.append(this.buttons.clearStorage);
-    this.buttons.clearStorage.onclick = this.handlers.clearStorage.bind(this);
+    // this.buttons.clearStorage.onclick = this.handlers.clearStorage.bind(this);
+    this.buttons.clearStorage.onclick = () => localStorage.clear();
 
     this.append(this.buttons.resetDB);
-    this.buttons.resetDB.onclick = this.handlers.resetDB.bind(this);
+    // this.buttons.resetDB.onclick = this.handlers.resetDB.bind(this);
+    this.buttons.resetDB.onclick = () => this.constructor.resetDB();
 
     this.append(this.buttons.syncDB);
-    this.buttons.syncDB.onclick = this.handlers.syncDB.bind(this);
-
-    this.handlers.syncDB.call(this);
+    // this.buttons.syncDB.onclick = this.handlers.syncDB.bind(this);
+    this.buttons.syncDB.onclick = () => this.constructor.syncDB(this.endpoint);
   }
 
   buttons = {
@@ -58,4 +59,20 @@ export default class extends DocumentFragment {
       ]);
     },
   };
+
+  static resetDB() {
+    [...idb.app.objectStoreNames].forEach((store) => idb.app.clear(store));
+  }
+
+  static async syncDB(endpoint) {
+    const res = await fetch(endpoint);
+    const data = await res.json();
+
+    const tx = idb.app.transaction('entries', 'readwrite');
+    await Promise.all([
+      tx.store.clear(),
+      ...data.map(((entry) => tx.store.put(entry))),
+      tx.done,
+    ]);
+  }
 }
